0c4d8add86018a53715e869c512f69ec0c46107a
Ronda D Cowen <cowenhome5@gmail.com>
2020-03-31 22:27:43 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,7 @@ class Song < ActiveRecord::Base
   def drake_made_this
 
     Artist.find_or_create_by(name: 'Drake')
-    self.artist = drake
+    self.artist.name = drake
     binding.pry
     # self.
 
